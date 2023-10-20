dac81f41a8b0a013b1609351654fe54ad6c72b53
Luis Leal <luisrrleal@gmail.com>
2023-10-19 19:56:38 -0600

    Finish ejercicio 5

--- Introducción-python/Ejercicio-5.py
+++ Introducción-python/Ejercicio-5.py
@@ -3,6 +3,15 @@ Ejercicio 5: Elaborar un script el cual
 1. Tenga una función llamada sumador_funcion(num_1, num_2)
 y reciba dos parámetros de entrada
 2. Retorne el valor de la suma.
-3. Imprimir en pantalla el resultado utilizando el comando
-format, con solo tres dígitos
+3. Imprimir en pantalla el resultado, con solo tres dígitos
 """
+
+
+def sumador_funcion(num_1, num_2):
+    return round(num_1+num_2, 3)
+
+
+num_1 = float(input("Ingrese número 1: "))
+num_2 = float(input("Ingrese número 2: "))
+
+print(f"{num_1}+{num_2} = {sumador_funcion(num_1, num_2)}")

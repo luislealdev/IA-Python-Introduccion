ba453f13a62de52e2e1bb627d9620282172d70e9
Luis Leal <luisrrleal@gmail.com>
2023-10-19 20:01:57 -0600

    Finish ejercicio 7

--- Introducción-python/Ejercicio-7.py
+++ Introducción-python/Ejercicio-7.py
@@ -6,3 +6,20 @@ parámetros de entrada
 mayor que o igual que.
 3. La función no retorna ningún valor
 """
+
+
+def comparador_funcion(num_1, num_2):
+    if (num_1 < num_2):
+        aux = "menor"
+    elif (num_1 > num_2):
+        aux = "mayor"
+    else:
+        aux = "igual"
+
+    print(f"El número {num_1} es {aux} al número {num_2}")
+
+
+num_1 = float(input("Ingrese número 1: "))
+num_2 = float(input("Ingrese número 2: "))
+
+comparador_funcion(num_1, num_2)
